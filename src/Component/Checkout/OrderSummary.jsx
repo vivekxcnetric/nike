76f0073592980d365ae52d7683d711dad646b8be
vivek <vivek.kumar@cnetric.com>
2025-01-14 12:13:14 +0530

    gg

--- src/Component/Checkout/OrderSummary.jsx
+++ src/Component/Checkout/OrderSummary.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchCart } from '../../redux/slices/cartSlice';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchCart } from "../../redux/slices/cartSlice";
 
 const cardClass = "w-full p-8 bg-white shadow-md rounded-md";
 const flexClass = "flex justify-between";
@@ -17,28 +17,40 @@ const OrderSummary = () => {
       dispatch(fetchCart());
     }
   }, [dispatch, token]);
-  if (cart?.id){
-    localStorage.setItem("cartId",cart?.id)
+  if (cart?.id) {
+    localStorage.setItem("cartId", cart?.id);
   }
   // console.log(cart)
-//   if (!cart || cart?.lines.length === 0) {
-//     return (
-//       <div className="flex justify-center">
-//         <p className="text-lg text-gray-600">No items in your order.</p>
-//       </div>
-//     );
-//   }
+  //   if (!cart || cart?.lines.length === 0) {
+  //     return (
+  //       <div className="flex justify-center">
+  //         <p className="text-lg text-gray-600">No items in your order.</p>
+  //       </div>
+  //     );
+  //   }
 
   return (
     <div className={cardClass}>
       <h2 className="text-lg font-semibold text-zinc-800">Order Summary</h2>
       <div className="mt-4">
-        <p className={flexClass}><span>Subtotal</span><span>₹ {cart?.subtotal.toLocaleString()||0}</span></p>
-        <p className={flexClass}><span>Delivery/Shipping</span><span>₹ {cart?.shipping||0}</span></p>
+        <p className={flexClass}>
+          <span>Subtotal</span>
+          <span>₹ {cart?.subtotal?.toLocaleString() || 0}</span>
+        </p>
+        <p className={flexClass}>
+          <span>Delivery/Shipping</span>
+          <span>₹ {cart?.shipping || 0}</span>
+        </p>
         <hr className="my-2" />
-        <p className={`${flexClass} font-bold`}><span>Total</span><span>₹ {(cart?.total || 0).toLocaleString()}</span></p>
+        <p className={`${flexClass} font-bold`}>
+          <span>Total</span>
+          <span>₹ {(cart?.total || 0).toLocaleString()}</span>
+        </p>
       </div>
-      <p className={`${itemClass} mt-1`}>(The total reflects the price of your order, including all duties and taxes)</p>
+      <p className={`${itemClass} mt-1`}>
+        (The total reflects the price of your order, including all duties and
+        taxes)
+      </p>
       {/* {step === 'payment' && (
         <button
           className="w-full mt-6 bg-black text-white py-3 rounded-lg hover:bg-gray-800"
@@ -47,14 +59,17 @@ const OrderSummary = () => {
           Checkout
         </button>
       )} */}
-      <h3 className="mt-4 font-semibold">Arrives {cart?.arrivesDate}</h3>   
+      <h3 className="mt-4 font-semibold">Arrives {cart?.arrivesDate}</h3>
 
       {cart?.lines?.map((item) => (
         <div key={item.id} className="flex items-start mt-4 space-x-4">
           <img
             aria-hidden="true"
             alt={`${item?.productVariant?.name} (UK Size ${item?.size})`}
-            src={item?.productVariant?.images?.[0]?.url|| item?.productVariant?.featuredAsset?.url}
+            src={
+              item?.productVariant?.images?.[0]?.url ||
+              item?.productVariant?.featuredAsset?.url
+            }
             className="w-32 h-32 object-cover rounded-md"
           />
           <div>
@@ -67,7 +82,6 @@ const OrderSummary = () => {
       ))}
 
       {/* Show the checkout button only when the step is 'payment' */}
-     
     </div>
   );
 };
